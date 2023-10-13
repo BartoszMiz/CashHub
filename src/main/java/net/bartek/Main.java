c7afd158df858cbd1ed0594b66fe80536120f48d
BartoszMiz <bartmiz.2004@gmail.com>
2023-10-13 16:22:36 +0200

    Initial commit

---
+++ src/main/java/net/bartek/Main.java
@@ -0,0 +1,15 @@
+package net.bartek;
+
+import net.bartek.logging.ConsoleLogger;
+import net.bartek.logging.ILogger;
+
+import java.net.*;
+import java.io.*;
+
+public class Main {
+	private static final int PORT = 8080;
+
+	public static void main(String[] args) {
+
+	}
+}

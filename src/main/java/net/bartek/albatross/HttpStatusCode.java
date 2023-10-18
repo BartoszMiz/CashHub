feaf257995a80c41a3bb88158edef2e05c656c08
BartoszMiz <bartmiz.2004@gmail.com>
2023-10-18 18:35:34 +0200

    Add HTTP status code enum

---
+++ src/main/java/net/bartek/albatross/HttpStatusCode.java
@@ -0,0 +1,22 @@
+package net.bartek.albatross;
+
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+public enum HttpStatusCode {
+	OK(200),
+	Created(201),
+	BadRequest(400),
+	Unauthorized(401),
+	Forbidden(403),
+	NotFound(404),
+	MethodNotAllowed(405),
+	ImATeapot(418),
+	TooManyRequests(429),
+	InternalServerError(500);
+
+
+	private final int code;
+
+	HttpStatusCode(int code) {
+		this.code = code;
+	}
+}

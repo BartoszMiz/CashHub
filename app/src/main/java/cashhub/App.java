e17fea2501186a53034aef1073bb773b0671d5c9
BartoszMiz <bartmiz.2004@gmail.com>
2023-11-17 13:42:14 +0100

    Add fromFile to HttpResponseBuilder

--- app/src/main/java/cashhub/App.java
+++ app/src/main/java/cashhub/App.java
@@ -4,6 +4,8 @@ import cashhub.albatross.*;
 import cashhub.logging.ConsoleLogger;
 import cashhub.logging.LogLevel;
 
+import java.io.IOException;
+
 public class App {
 	private static final int PORT = 8080;
 
@@ -11,6 +13,22 @@ public class App {
 		var logger = new ConsoleLogger(LogLevel.Debug);
 
 		var router = new Router(logger);
+		router.addRoute(HttpVerb.GET, "/", request -> {
+			try {
+				return HttpResponseBuilder.create()
+						.fromFile("/index.html")
+						.build();
+			} catch(IOException e) {
+				logger.LogError(String.format("Failed to read file %s: %s", request.url(), e.getMessage()));
+				return HttpResponseBuilder
+						.create()
+						.withDefaultHeaders()
+						.withStatusCode(HttpStatusCode.NotFound)
+						.build();
+			}
+
+		});
+
 		router.addRoute(HttpVerb.GET, "/helloworld", request -> HttpResponseBuilder.create()
 			.withStatusCode(HttpStatusCode.OK)
 			.withDefaultHeaders()

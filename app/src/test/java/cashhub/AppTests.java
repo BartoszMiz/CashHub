d4bb29022ead1097a830dfc893c3fd3cf56bee43
BartoszMiz <bartmiz.2004@gmail.com>
2023-12-31 16:32:32 +0100

    Add first dummy test

---
+++ app/src/test/java/cashhub/AppTests.java
@@ -0,0 +1,11 @@
+package cashhub;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTests {
+	@Test void testsWork() {
+		var sut = new App();
+		assertTrue(true, "Tests work!");
+	}
+}
